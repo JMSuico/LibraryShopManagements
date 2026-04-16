66434e93cc900db73d49679043cbc4f4a61cf1de
JM Suic0 <suicojm99@gmail.com>
2026-04-16 13:55:34 +0800

    triggering refresh

--- LibraryShopManagement/obj/Debug/net9.0/LibraryShopManagement.AssemblyInfo.cs
+++ LibraryShopManagement/obj/Debug/net9.0/LibraryShopManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryShopManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73d553cddceb371be032cba01fbc2d3ed4eb4b4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryShopManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryShopManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
